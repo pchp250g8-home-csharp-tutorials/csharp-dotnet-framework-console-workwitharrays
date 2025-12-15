3f4efbd5603a79546050754d7da06de0235b17de
Alexander <pchp250g8@tutamail.com>
2025-12-15 14:56:03 +0300

    Добавьте файлы проекта.

---
+++ WorkWithArrays/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WorkWithArrays
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Clear();
+            Console.WriteLine("Input a count of elements in array");
+            Int64.TryParse(Console.ReadLine(), out var nElems);
+            var iArray = new int[nElems];
+            for (int i = 0; i < nElems; i++)
+            {
+                Console.WriteLine("Input an element N {0}", i + 1);
+                int.TryParse(Console.ReadLine(), out iArray[i]);
+            }
+            Console.WriteLine("Inputed array:");
+            for (int i = 0; i < nElems; i++)
+            {
+                Console.Write(iArray[i] + " ");
+            }
+            Console.Read();
+        }
+    }
+}
